0126241be07c8da6f8b4a6393dda02c5339eaaf9
safi miazi <mohibullamiazi@gmail.com>
2025-04-09 11:33:30 +0600

    Refactor hello.c to include arithmetic operations and remove user input; add empty input.txt and output.txt files.

--- hello.c
+++ hello.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
-
 int main() {
-    int a;
-    int b;
-    scanf("%d", &a);
-    scanf("%d", &b);
-    printf("%d\n", b + a);
+int a = 5;
+int b= 10;
+int sum = a + b;
+printf("The sum = %d\n", sum);
+int substract = a - b;
+printf("the substract = %d\n", substract);
+int multiply = a * b;
+printf("the multiply = %d\n", multiply);
+
+float divide = a / b;
+printf("the divide = %f\n", divide);
+int mod = a % b;
+printf("the mod = %d\n", mod);
+return 0;
 }
