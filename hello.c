6b29b788f890366b8510a4e3f701d9a78601053c
safi miazi <mohibullamiazi@gmail.com>
2025-04-16 09:22:30 +0600

    Refactor hello.c to implement a passcode check; update input.txt and output.txt with new test values and expected results

--- hello.c
+++ hello.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-int main()
-{
-    int N;
+int main(){
 
-    scanf("%d", &N);
-    if (N > 1000)
+    int pass;
+    while (1)
     {
-        printf("I will buy Punjabi\n");
-        if (N - 1000 >= 500)
-        {
-            printf("I will buy new shoes\n");
-            printf("Alisa will buy new shoes\n");
+        scanf("%d", &pass); // fixed line
+
+        if(pass == 1999){
+            printf("Correct\n");
+            break;
+        } else {
+            printf("Wrong\n");
         }
     }
-    else
-    {
-        printf("Bad luck!\n");
-    }
+
     return 0;
 }
