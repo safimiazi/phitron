35b656a701304c38badfae3b986c1017aface478
safi miazi <mohibullamiazi@gmail.com>
2025-04-18 10:13:59 +0600

    Refactor hello.c to initialize sum and calculate the sum of user-inputted numbers; update input.txt and output.txt accordingly

--- hello.c
+++ hello.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 int main()
 {
+int n ;
+scanf("%d", &n);
+int a[n];
+int sum;
+for (int i = 0; i < n; i++)
+{
+    /* code */
+    scanf("%d", &a[i]);
+    sum += a[i];
+}
+printf("%d", sum);
+
 
  
 }
