c090de053986447911dd70bd7f639cf063f768e5
safi miazi <mohibullamiazi@gmail.com>
2025-04-18 21:26:10 +0600

    Refactor hello.c to use a temporary variable for input; update input.txt and output.txt with new test values and expected results

--- hello.c
+++ hello.c
@@ -3,14 +3,18 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    int a[n];
-    int sum = 0;
+  long long  int sum = 0;
+
     for (int i = 0; i < n; i++)
     {
-        /* code */
-        scanf("%d", &a[i]);
-        sum += a[i];
+        int num;
+        scanf("%d", &num);
+        sum += num;
+    }
+    if (sum < 0)
+    {
+        sum = -sum;
     }
 
-    printf("%d", sum);
+    printf("%lld\n", sum);
 }
