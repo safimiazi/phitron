56441249f1961664741f6e5edcd24edfd5bbbe45
safi miazi <mohibullamiazi@gmail.com>
2025-04-18 11:59:33 +0600

    Refactor hello.c to initialize sum correctly; update input.txt and output.txt with new test values and expected results

--- hello.c
+++ hello.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 int main()
 {
-int n ;
-scanf("%d", &n);
-int a[n];
-int sum;
-for (int i = 0; i < n; i++)
-{
-    /* code */
-    scanf("%d", &a[i]);
-    sum += a[i];
-}
-printf("%d", sum);
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
 
-
- 
+    printf("%d", sum);
 }
